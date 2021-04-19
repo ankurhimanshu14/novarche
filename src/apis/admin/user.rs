699169de32fb3da63620f5be23898a9bab0c6ff9
Ankur Himanshu <ankurhimanshu1408@gmail.com>
2021-04-19 12:54:34 +0530

    user push working

--- src/apis/admin/user.rs
+++ src/apis/admin/user.rs
@@ -1,5 +1,6 @@
 pub mod user {
     use serde::{ Serialize, Deserialize};
+    use std::io::Read;
     use chrono::{ DateTime, Utc };
     use bcrypt::{ hash, DEFAULT_COST };
     use mysql::*;
@@ -11,8 +12,8 @@ pub mod user {
         pub username: String,
         password: String,
         pub role: String,
-        created_at: DateTime<Utc>,
-        modified_at: Option<DateTime<Utc>>
+        // created_at: DateTime<Utc>,
+        // modified_at: Option<DateTime<Utc>>
     }
 
     impl User {
@@ -56,10 +57,55 @@ pub mod user {
                         .trim_end_matches("\r\n")
                         .to_string(),
 
-                created_at: Utc::now(),
+                // created_at: Utc::now(),
 
-                modified_at: None
+                // modified_at: None
             }
+        }
+
+        pub fn push(u: User) -> Result<()> {
+            let table = "CREATE TABLE IF NOT EXISTS user(
+                id          INT             NOT NULL            PRIMARY KEY AUTO_INCREMENT,
+                email       VARCHAR(50)     NOT NULL,
+                username    VARCHAR(20)     NOT NULL            UNIQUE,
+                password    VARCHAR(100)    NOT NULL,
+                role        VARCHAR(20)     NOT NULL
+            ) ENGINE = InnoDB;";
+
+            let insert = "INSERT INTO user(email, username, password, role) VALUES (?, ?, ?, ?);";
+
+            let url = "mysql://root:@localhost:3306/mws_database";
+
+            let pool = Pool::new(url)?;
+
+            let mut conn = pool.get_conn()?;
+
+            conn.query_drop(table)?;
+            conn.exec_drop(insert, (u.email, u.username, u.password, u.role)).unwrap();
+            Ok(())
+        }
+
+        pub fn get<T: FromRow>() -> Result<()> {
+            let query = "SELECT * FROM user;";
+
+            let url = "mysql://root:@localhost:3306/mws_database";
+
+            let pool = Pool::new(url)?;
+
+            let mut conn = pool.get_conn()?;
+
+            let json = conn.query_map(query, |(email, username, password, role)| {
+                User {
+                    email,
+                    username,
+                    password,
+                    role
+                }
+            })?;
+
+            println!("{:#?}", json);
+
+            Ok(())
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ mod apis;
 use crate::apis::admin::user::user::User;
 
 fn main() {
-    let new_user = User::new();
-    println!("{:#?}", new_user);  
+    // let new_user = User::new();
+    // User::push(new_user);
+    User::get();
+
+    // println!("{:#?}", all_users);
 }
