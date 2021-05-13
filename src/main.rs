1bad6a415df3b5de3f050f3c544fcbde7658a022
Ankur Himanshu <ankurhimanshu1408@gmail.com>
2021-05-13 17:20:11 +0530

    Working on part

--- src/main.rs
+++ src/main.rs
@@ -32,11 +32,6 @@ use apis::{
 
 fn main() {
 
-    let new_part = Part::new(8001, "FD GEAR".to_string(), "SAE 8620H".to_string(), 7.8, 8.2, "ISOTHERMAL ANNEALED".to_string(), 'A'.to_string(), "23-10-2020".to_string())
-        .post().unwrap();
-
-    println!("{:?}", new_part);
-
     let mut siv = Cursive::new();
 
     siv.add_layer(Menubar::new());
@@ -411,6 +406,93 @@ fn main() {
                         .leaf(
                             "New",
                             |s| {}
+                        )
+                )
+        )
+        .add_subtree(
+            "Engineering",
+            menu::MenuTree::new()
+                .subtree(
+                    "Part Master",
+                    menu::MenuTree::new()
+                        .leaf(
+                            "New",
+                            |s| {
+                                let grd = Grades::get().unwrap();
+
+                                s.add_layer(
+                                    Dialog::new()
+                                        .title("Add new part")
+                                        .padding_lrtb(1,1,1,0)
+                                        .content(
+                                            ListView::new()
+                                                .child("Part No", EditView::new().with_name("part_no").fixed_width(30))
+                                                .child("Part Name", EditView::new().with_name("part_name").fixed_width(30))
+                                                .child(
+                                                    "Material Grade",
+                                                    SelectView::new()
+                                                        .popup()
+                                                        .h_align(HAlign::Center)
+                                                        .autojump()
+                                                        .with_all_str(grd)
+                                                        .on_select(|s, item| {
+                                                            println!("{}", &item)
+                                                        })
+                                                        .with_name("grade")
+                                                        .fixed_width(30))
+                                                .child("Forging Weight", EditView::new().with_name("forging_wt").fixed_width(30))
+                                                .child("Cut Weight", EditView::new().with_name("cut_wt").fixed_width(30))
+                                                .child("Delivery Condition", EditView::new().with_name("del_cond").fixed_width(30))
+                                                .child("Drawing Revision No", EditView::new().with_name("drawing_rev_no").fixed_width(30))
+                                                .child("Drawing Revision Date", EditView::new().with_name("drawing_rev_date").fixed_width(30))
+                                        )
+                                        .button(
+                                            "Add",
+                                            |s| {
+                                                let part_no = s.call_on_name("part_no", |v: &mut EditView| {
+                                                    v.get_content()
+                                                }).unwrap();
+                                                let part_name = s.call_on_name("part_name", |v: &mut EditView| {
+                                                    v.get_content()
+                                                }).unwrap();
+                                                let grade = s.call_on_name("grade", |v: &mut SelectView| {
+                                                    v.selection()
+                                                }).unwrap();
+                                                let forging_wt = s.call_on_name("forging_wt", |v: &mut EditView| {
+                                                    v.get_content()
+                                                }).unwrap();
+                                                let cut_wt = s.call_on_name("cut_wt", |v: &mut EditView| {
+                                                    v.get_content()
+                                                }).unwrap();
+                                                let del_cond = s.call_on_name("del_cond", |v: &mut EditView| {
+                                                    v.get_content()
+                                                }).unwrap();
+                                                let drawing_rev_no = s.call_on_name("drawing_rev_no", |v: &mut EditView| {
+                                                    v.get_content()
+                                                }).unwrap();
+                                                let drawing_rev_date = s.call_on_name("drawing_rev_date", |v: &mut EditView| {
+                                                    v.get_content()
+                                                }).unwrap();
+
+                                                match Part::new(
+                                                    part_no.parse::<usize>().unwrap(),
+                                                    part_name.to_string(),
+                                                    grade.unwrap().to_string(),
+                                                    forging_wt.parse::<f32>().unwrap(),
+                                                    cut_wt.parse::<f32>().unwrap(),
+                                                    del_cond.to_string(),
+                                                    drawing_rev_no.to_string(),
+                                                    drawing_rev_date.to_string()
+                                                ).post() {
+                                                    Ok(_) => s.add_layer(Dialog::text("New Part added successfully").button("Ok", |s| {s.quit()})),
+                                                    Err(e) => s.add_layer(Dialog::text(format!("Error encountered: {}", e)).button("Ok", |s| {s.quit()}))
+                                                }
+
+                                            }
+                                        )
+                                        .dismiss_button("Cancel")
+                                )
+                            }
                         )
                 )
         )
